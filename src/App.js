41d7c5a4a51662ddfc63ad822240aa75377409e0
Kamzen <themba.makamu@cdasolutions.co.za>
2022-08-01 20:09:33 +0200

    Google Auth Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import jwt_decode from 'jwt-decode';
 
 function App() {
+  const handleCallBack = (response) => {
+    console.log(response);
+    const userInfo = jwt_decode(response.credential);
+
+    console.log(userInfo)
+  };
+
+  const [init, setInit] = useState(false);
+  /* global google */
+  useEffect(() => {
+    (async () => {
+        // eslint-disable-next-line no-unused-expressions, no-undef
+        await google.accounts.id.initialize({
+          client_id:
+            "1020009104978-c9rsv08g134lkva3s6vdva9sm1eq2tpl.apps.googleusercontent.com",
+          callback: handleCallBack,
+        });
+        setInit(true);
+
+      if (init) {
+        // eslint-disable-next-line no-unused-expressions, no-undef
+        google.accounts.id.renderButton(
+          document.getElementById('singin'), {
+          theme: "outline",
+          size: "small",
+        });
+      }
+      google.accounts.id.prompt()
+    })();
+  }, [init]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="singin" style={{height : '40px'}}>
+        Sign In With Google
+      </div>
     </div>
   );
 }
